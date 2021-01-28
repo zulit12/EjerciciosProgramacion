b2f8392d2f2563c4bc57d16fd62e32e6e7d3baf2
Zulit <49183495+zulit12@users.noreply.github.com>
2021-01-28 14:58:18 +0100

    ADD Math

--- src/capitulo4/recursos/Math/CalcularPiSeriesInfinitas.java
+++ src/capitulo4/recursos/Math/CalcularPiSeriesInfinitas.java
@@ -4,6 +4,20 @@ public class CalcularPiSeriesInfinitas {
 
 	public static void main(String[] args) {
 		
+	// Math.Pi =  (4/1)-(4/3)+(4/5)-(4/7)+(4/9)-(4/11)+(4/13)-(4/15)
+		
+	double calculo;	
+	double x = 3;
+	float piCalculado = 4;
+	int signo = -1;	
+	do {
+		piCalculado += signo * 4.0 / x;
+		signo *= -1;
+		x += 2;
+
+	} while (Math.abs(Math.PI - piCalculado) >= 0.00001);	
+	System.out.println(piCalculado);
+		
 	}
 
 }
