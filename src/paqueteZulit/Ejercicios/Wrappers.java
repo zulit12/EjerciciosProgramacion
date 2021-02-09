a8094db213e24b9db2b001136f604984130535ab
Zulit <Zulit@DESKTOP-MOGN7CQ>
2021-02-09 18:02:38 +0100

    ADD Wrappers

--- src/paqueteZulit/Ejercicios/Wrappers.java
+++ src/paqueteZulit/Ejercicios/Wrappers.java
@@ -1,8 +1,10 @@
 package paqueteZulit.Ejercicios;
 
+import javax.swing.JOptionPane;
+
 public class Wrappers {
 
-	public static void main(String[] args) {
+	public static void limiteMaximoMinimo() {
 		
 		System.out.println("Valor m�ximo para un Integer: " + Integer.MAX_VALUE);
 		System.out.println("Valor m�nimo para un Integer: " + Integer.MIN_VALUE);
@@ -22,6 +24,69 @@ public class Wrappers {
 		System.out.println("\nValor m�ximo para un Byte: " + Byte.MAX_VALUE);
 		System.out.println("Valor m�nimo para un Byte: " + Byte.MIN_VALUE);
 		
+	}
+	
+	public static void contraseña() {
+		
+		boolean mayusculas = false;
+		boolean minusculas = false;
+		boolean digito = false;
+		boolean caracterNoAlfa = false;
+		
+		
+		do {
+			String contrasena = JOptionPane.showInputDialog("Introduzca una contraseña con los siguientes requisitos: " + "\n"+
+															"Al menos una mayuscula" + "\n"+
+															"Al menos una minuscula" + "\n"+
+															"Al menos un digito"     + "\n"+
+															"Al menos un caracter no alfanumerico"); 
+			char cadena[] = contrasena.toCharArray();
+			
+			for (int i = 0; i < cadena.length; i++) {
+			
+				if (mayusculas == false) {
+					mayusculas = Character.isUpperCase(cadena[i]);
+				}
+				
+				if (minusculas == false) {
+					minusculas = Character.isLowerCase(cadena[i]);
+				}
+				
+				if (digito == false) {
+					digito = Character.isDigit(cadena[i]);
+				}
+				
+				if (caracterNoAlfa == false) {
+					caracterNoAlfa = Character.isLetterOrDigit(cadena[i]);
+				}
+				
+			}
+			
+			if (mayusculas == false || minusculas == false || digito == false || caracterNoAlfa == false) {
+				System.out.println("No se han cumplido los requisitos");
+			}
+			
+		} while (mayusculas == false || minusculas == false || digito == false || caracterNoAlfa == false);
+		System.out.println("Contraseña valida");
+	}
+	
+	public static void numerosHexadecimal() {
+		
+		for (int i = 0; i <= 100; i++) {
+
+            String hex = Integer.toHexString(i);
+
+            System.out.println("El hexadecimal de " + i + " es: " + hex);
+
+        }
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		//limiteMaximoMinimo();
+		//contraseña();
+		numerosHexadecimal();
 	}
 
 }
